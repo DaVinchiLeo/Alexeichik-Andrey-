e345cf7a40b646311565178a13dd702df648a0bf
Da_Vinchi <silver199718@mail.ru>
2021-10-30 11:38:17 +0300

    array with git commands

---
+++ 1/script.js
@@ -0,0 +1,6 @@
+let array = ["git add .", "git commit -m `commit title`", 
+"git push origin `branch name`", "git log", "git checkout -b newbranch"];
+for (let i = 0; i < array.length; i++) {
+  alert(array[i]);
+}
+
