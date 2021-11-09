66b529032b9f38e8ef76eb084d093a992af656e0
Da_Vinchi <silver199718@mail.ru>
2021-11-09 18:47:16 +0300

    DZ3

--- 3/script.js
+++ 3/script.js
@@ -1,46 +1,13 @@
 let array = [[5, 7, [4, [2], 8, [1, 3], 2], [9, []], 1, 8]];
+let sum = 0;
 
-/*
+
 let functionArray = (array) => {
-  let result=0;
-  for (let i = 0; i < array.length; i++) {
-    result+= array[i];
-  }
-  alert(`сумма массива=${result}`);
-  }
-  functionArray(array);
-  
-*/
-
-/*
-let functionArray = (array) => {
-  let result = 0;
-
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      result += array[i][j];
-    }
-    alert(`сумма массива=${result}`);
-  }
-}
-functionArray(array);*/
-
-
-/*--------------------------------1метод-----------------------------------------------*/
-
-/*const sum = array => array.reduce((res, el) => res + (Array.isArray(el) ? sum(el) : el), 0);
-
-alert(sum(array));*/
-/*--------------------------------2метод-----------------------------------------------*/
-var sum = 0;
-
-function foo(array) {
-
-  array.forEach(function(value, index) {
-    Array.isArray(value) ? foo(value) : sum += value; 
+  array.forEach(function (value) {
+    Array.isArray(value) ? functionArray(value) : (sum += value);
   });
 
   return sum;
-}
+};
 
-alert(foo(array));
+alert(`сумма массива=${functionArray(array)}`);
