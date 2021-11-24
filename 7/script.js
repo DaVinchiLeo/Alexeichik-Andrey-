a612f631acd87b0468b92c49e6fccef584650874
Da_Vinchi <silver199718@mail.ru>
2021-11-24 19:35:10 +0300

    dz7

--- 7/script.js
+++ 7/script.js
@@ -1,24 +1,18 @@
 /*№7. Добавить функцию из 4 домашки каждоой строке. Чтобы можно было вызвать как 
 "Привет".getVowels(). Замыкание можно убрать и оставить просто подсчет гласных*/
 
-
-function SearchForLetters(params1) {
+String.prototype.SearchForLetters=function() {
+   
   let SumOfLetters = 0;
      let params2 = ["а", "о", "и", "е", "ё", "э", "ы", "у", "ю", "я"];
-      for (let i of params1.toLowerCase()) { //если параметр 2 не одна буква
+      for (let i of this.toLowerCase()) { 
         if (params2.includes(i)) {
           console.log(i)
           SumOfLetters += 1;
-
         }
-      }
+      };
       alert(`Сумма всех гласных ${SumOfLetters}`);
       return SumOfLetters;
-    }
+    };
+    "привет как дела(5)".SearchForLetters();
 
-  
-
-let mystring = prompt(`Введите строку для поиска и подсчета согласных`);
-
-
-SearchForLetters(mystring);
