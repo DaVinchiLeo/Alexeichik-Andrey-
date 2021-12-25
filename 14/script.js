c27807e845f4ee65efa4b8f075ba817bac57b5d8
Da_Vinchi <silver199718@mail.ru>
2021-12-25 16:27:27 +0300

    add DZ14 (one mistake)

--- 14/script.js
+++ 14/script.js
@@ -10,44 +10,39 @@ let but = document.getElementById("submit");
 but.addEventListener("click", function () {
   let log = document.getElementById("login");
   let pas = document.getElementById("password");
-
   if (log.value.length < 5) {
     alert("Логин <5");
     return;
   }
+  let newpas =pas.value.split('');
+  alert(newpas);
+  console.log(newpas);
 
   if (pas.value.length < 5) {
     alert("пароль <5");
     return;
   }
-  /*
-  console.log(pas.value.split(""));
-  debugger;
 
-  for (let char of pas.value) {
-    if (isNaN(char) != true) {
-      debugger;
+  for (let char of newpas) {
+    if (isNaN(char) !== true) {
+      alert("нет букв");
+      return;
     }
-    console.log("нет цифр");
-  }
-  for (let char of pas.value) {
     if (isNaN(char) === true) {
-      debugger;
+      alert("нет цифр");
+      return;
     }
-    console.log("нет букв");
   }
-*/
-  localStorage.setItem("log+pas", JSON.stringify(log.value + pas.value));
+  
+  localStorage.setItem("log+pas", JSON.parse(JSON.stringify(log.value + pas.value)));
   let value = localStorage.getItem("log+pas");
-  console.log(JSON.parse(value));
-  console.log(localStorage)
+  alert(JSON.parse(value));
 });
 if (localStorage.getItem("log+pas") !== null) {
-  let p=document.createElement('p')
-  let img=document.createElement('img')
-document.body.append(p);
-document.body.append(img);
-p.innerText="Хорошо логин и пароль знаком";
-img.src="http://risovach.ru/thumb/upload/200s400/2016/03/mem/nelzya-prosto-tak-vzyat-i-boromir-mem_107428305_orig_.jpg?eju0t"
+  let p = document.createElement("p");
+  let img = document.createElement("img");
+  document.body.append(p);
+  document.body.append(img);
+  p.innerText = "Хорошо логин и пароль знаком";
+  img.src = "http://risovach.ru/thumb/upload/200s400/2016/03/mem/nelzya-prosto-tak-vzyat-i-boromir-mem_107428305_orig_.jpg?eju0t";
 }
-
